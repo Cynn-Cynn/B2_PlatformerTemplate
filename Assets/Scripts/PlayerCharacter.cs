7f9faaac7ab3c3ec69c79622571502b73f4cd4ad
Cynthia <cynthiabon.perso@gmail.com>
2025-09-18 15:06:10 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public void Movement(float input)
+    {
+
+    }
+
+    public void StartJump()
+    {
+
+    }
+
+    public void ActionOne()
+    {
+
+    }
+
+    public void ActionTwo()
+    {
+
+    }
+}
